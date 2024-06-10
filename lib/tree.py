759dcbb0854418ee07f22ddfb53830b9efbd12aa
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-06-10 10:49:50 +0300

    complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,19 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if not self.root:
+            return None
+
+        stack = [self.root]
+        
+        while stack:
+            node = stack.pop()
+            if node['id'] == id:
+                return node
+            
+            for child in reversed(node.get('children', [])):
+                stack.append(child)
+        
+        return None
